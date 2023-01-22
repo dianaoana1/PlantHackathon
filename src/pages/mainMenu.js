9a3789acabdebba5969d171db607a06838decb67
crisscrosstheroad <86334398+crisscrosstheroad@users.noreply.github.com>
2023-01-21 22:22:32 -0500

    changes to app and main menu

--- src/pages/mainMenu.js
+++ src/pages/mainMenu.js
@@ -1,21 +1,43 @@
-// this will be the main page displaying the plants and their watering schedules
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function welcomePage(){
-    return (
-        <View style={StyleSheet.container}>
-            <Text>Hello from welcome Page</Text>
-            <StatusBar style='auto'/>
-        </View>
-    );
+function MyGarden() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>My Garden</Text>
+      </View>
+      <View style={styles.mainContainer}>
+        {/* Add your content here */}
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  headerContainer: {
+    margin: 20,
+    width: 200,
+    height: 50,
+    backgroundColor: '#90F694',
+    borderRadius: 15,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    color: '#fff',
+    fontSize: 20,
+    fontStyle: 'italic',
+    fontWeight: '500',
+  },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#E8F5E9',
+  },
 });
+
+export default MyGarden;
