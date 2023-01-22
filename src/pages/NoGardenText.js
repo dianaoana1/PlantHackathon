5ccee48c62126f48e43b50634ed4d2942da4f8e7
Jasmine Romain <jasmine.dream09@gmail.com>
2023-01-21 22:06:47 -0500

    final noGardenText

--- src/pages/NoGardenText.js
+++ src/pages/NoGardenText.js
@@ -1,20 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import MyButton from '../components/openingPageButton.js';
 
-export default function NoGardenTexts(){
+export default function NoGardenTexts() {
     return (
-        <View style={styles.container}>
-            <Text>You do not have any plans yet !</Text>
-            <StatusBar style='auto'/>
-        </View>
+      <View style={styles.container}>
+        <Text style={styles.text}>You do not have any plants yet !</Text>
+        <MyButton
+          text=" Add plant + "
+          navigateTo="mainMenu"
+          buttonStyle={styles.button}
+          color="#fff"
+        />
+        <StatusBar style="auto" />
+      </View>
     );
-}
+  }
 
-const styles = StyleSheet.create({
+  const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+    },
+    text: {
+        padding: 26,
+        fontSize: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        alignSelf: 'center',
     },
 });
